6abd7ab50c425fd967c154ddef41de31e8c8a7f8
Anna-Shum-ui <anya.fredy@gmail.com>
2025-05-18 17:18:07 +0200

    Add working HTML, CSS and JS code

---
+++ script.js
@@ -0,0 +1,5 @@
+const answers = ["Yes", "No", "Maybe", "Try again"];
+const getAnswerButton = document.getElementById("getAnswerButton");
+const resultDiv = document.getElementById("result");
+const adviceDiv = document.getElementById("advice");
+
