c4a612b527d371f2a70a863c12c4c60fe2b30714
Anna-Shum-ui <anya.fredy@gmail.com>
2025-05-22 17:30:26 +0200

    Added logic to generate random answers on button click. Used addEventListener for event handling and trim() to check for empty input.

--- script.js
+++ script.js
@@ -1,5 +1,25 @@
 const answers = ["Yes", "No", "Maybe", "Try again"];
-const getAnswerButton = document.getElementById("getAnswerButton");
-const resultDiv = document.getElementById("result");
-const adviceDiv = document.getElementById("advice");
 
+const answerGenerator = {
+    getRandomAnswer : function() {
+        const index = Math.floor(Math.random() * 5);
+    return answers[index];
+    }
+    
+};
+
+const button = document.getElementById("getAnswerButton");
+const result = document.getElementById("result");
+const advice = document.getElementById("advice");
+
+button.addEventListener("click", function() {
+  const question = document.getElementById("questionInput").value;
+
+ if (question.trim() === "") {
+    result.textContent = "Please type a question!";
+    return;
+  }
+
+  const randomAnswer = answerGenerator.getRandomAnswer();
+  result.textContent = "Answer: " + randomAnswer;
+});
